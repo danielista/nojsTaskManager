255362a882ecf58baeeb6ddbb9562e4be5a8e569
danielista <61905525+danielista@users.noreply.github.com>
2021-04-22 12:43:40 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+const { query } = require('express')
+const express = require('express')
+const mongodb = require('mongodb')
+
+const connectionURL = 'mongodb://localhost:27017'
+const databaseName = 'tskmngr'
+
+const app = express()
+const MongoClient = mongodb.MongoClient
+
+app.get('',(req,res) => {
+    res.send('Hello, I am your NODEJS serverik!')
+
+})
+
+app.listen(3000, ()=>{
+    console.log("Server beží na porte 3000 ;) ")
+})
+
+
+app.get('/about',(req,res) => {
+    res.send('tu už returnem html code<br><h1>AJAJAJ</h1>')
+
+})
+
+app.get('/task',(req,res) => {
+    MongoClient.connect(connectionURL,(error,client)=>{
+        if(error){
+            return console.log('NEDA sa pripojiť do databazy kámo!')
+        }
+        console.log('úspech s pripojením do monga ;)')
+
+        let filter = {};
+        if(req.query.done){
+            if(req.query.done=='true'){
+                filter = {done:true}
+            }else{
+                filter = {done:false}
+            }
+        }
+
+        if(req.query.priority){
+            if(req.query.priority!=isNaN){
+                cislo = (req.query.priority)
+                console.log(cislo)
+                filter = {priority:cislo}
+            }else{
+                return console.log('NEDA sa ')
+            }
+        }
+
+        const db = client.db(databaseName);
+        db.collection('tasks').find(filter).toArray((err,result)=>{
+            if (err) throw err;
+            console.log(result);
+            res.send(result)
+        }) 
+    })
+})
